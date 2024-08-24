cc5a7f6612d389a6b2e787696a1d78afb2c2a6cf
mikurikuri11 <mikuriyakenta003@gmail.com>
2024-08-24 19:31:22 +0900

    🗃️ Articleモデルの作成とテスト実装

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
+  has_many :articles, dependent: :destroy
 end
